d03a51b0ee2733584d7399b2ba8fcc86d2928f66
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-01-31 00:42:08 +0300

    Tokens.py

---
+++ TOKEN MAKER/Tokens.py
@@ -0,0 +1,47 @@
+<init token>
+     print % script_ $local$_c
+     print % (data.py($local$_c.script();
+          print % py(cs).local
+       $_TOKEN_$
+          ProgramConsole.py(script.($local.data
+          print % val(Hello World)
+         print % val(Hello World)
+            print % test val(Hello World)
+                   
+             Token.py(script.HelloWorld(isTest)
+       Token.py( script % custom-name(hello world.code(script)
+               init.port % custom.name ( hello world.) Newcodebutton.html
+      
+           token.init( % port % local ( == token
+           py(init.token % port % token == limit
+                  Hector.ip(script. % token == ip
+           getportbutton.html % == create token.ip(local:;00.0.01
+           0;20.0:10.01.0 % == token.ip(00;:10;01-2.token.ip
+                token enter == ifEnterIP.00.0(local.ip(;00.01.01.0.1.init
+          Token == localdataport. == Token print % token
+        Token. == Token.teleport.(script.allval(edit.local(name)
+             % extract port % == (script.val(edit.local;01.0.0?0($)
+             Token.button. == port val(script.github(json.da $ token
+                         Token == port val(host)
+          Token == val(host).token == port
+                              Ip.token.(ip == % port.json $ token
+        New token == hostval(token) == 0.0.0.0(ip)
+        New token == web(token) == ip.internet;09.0
+       New Token == valToken( print.data(script % token.json
+              Token hello.valToken 
+        New Token == valToken( script.json % token.json
+                 New Token == Token % token valToken( print="assets.animation
+          Example Token == assets/video.token.json % thistoken.ip(json.video.animation.java
+          Token.object(animation project. % token json. Token % print.data)
+          Token.object(animation % print.video(character.thisData.3d(json % token)
+           Web.ip token == % json (animation == token % Data. Print folder. % token).token.(customFolderif)
+         Example=" script. % json == map
+     Token == ip - example % token == map (animation.work(filetype.allfileanimation)
+      Token == allfileanimation % ip % token
+        Extract.token == script % token % assets token.www)
+
+ 
+
+ 
+
+             
