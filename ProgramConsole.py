94b788d5c72bb0ab6f5af18419042f9effa07650
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-01-31 00:12:43 +0300

    ProgramConsole

---
+++ ProgramConsole.py
@@ -0,0 +1,23 @@
+<init py>
+      print % data (script.data 
+      print % ProgramConsole == init.local
+           _$TOKEN$_
+        Console.ip(script.data(filename.make)
+            print % data == console.file(maker)
+        print % $_script
+              print(local.script.filename(createfilename(api)
+        print % token == filename="CreateFileName"
+             Button == token == filename.builder( %_script
+        print % token == lexer == token.assembly(data)
+       print % local(0.0.0.0.0)
+              print console.ip(token.logo(assembly.name)
+      
+         print token="0.0.0.0.0"
+         print comfy="0.0.0.0.0"
+                print % lexer == programming.data(script == )
+            
+       // Value data // 
+              print % thisData == lexer=(programming == data)
+        // RAM PB //
+                token="script(RAM.1MB="10,000.lines("
+                token="ram.ram(script 1MB="103MB"
