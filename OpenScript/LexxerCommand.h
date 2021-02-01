a10bfcd444c24a38264538c230da848e4e5187b0
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-01 21:32:39 +0300

    Lexer

---
+++ OpenScript/LexxerCommand.h
@@ -0,0 +1,14 @@
+#include <view>
+#include <data>
+#include <programminglanguage>
+   struct(ram == script)
+      data (ram == view
+              lexer.init(py)
+         view.comfy == view.py(ram)
+#include <script>
+          iostream-complex( == py
+     init(ip.host.data == ( == py
+         confirm.view($, script_token % print % data
+    init(ram.host(view.((viewer.py
+                  coding((view.py % token
+       print $.local(ascript( == INITPY
