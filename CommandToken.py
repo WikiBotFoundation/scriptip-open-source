32fee97e8173263eac8f70231204f34bef9ee0be
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-08 11:38:00 +0300

    Create CommandToken.py

---
+++ CommandToken.py
@@ -0,0 +1,6 @@
+if(null.charm(py.atom
+    enteredTextCreate.atom.(val)
+          if(token.create(val)
+   commandColor.token(setColor="enterColor"
+        token.val(is created text.(enter symbol""
+        EnterCreateText.api(color
