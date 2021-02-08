f301acd0137102a2347ff6aca0fe731def563158
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-08 11:34:56 +0300

    Create command.py

---
+++ command.py
@@ -0,0 +1,12 @@
+if(else == null).programminglanguageManifest
+   java.py(!null.console
+
+    ##TOKENS##
+   if(else.command.token(py.command
+    command 
+   print(enterCommand))
+           if(java.py!null.cha(token.java)
+        html.s(rec(data?.programmingConsole
+    if(else == name.command(({if.command(this.
+          programmingManifest.if@{token)
+  
