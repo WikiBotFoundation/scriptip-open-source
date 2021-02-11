4a5372ce2ce200d29e261fd22c026b2fda7c40f0
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-11 11:57:06 +0300

    Create server.java

---
+++ server/server.java
@@ -0,0 +1,22 @@
+import.os
+import.token
+import.java
+    @Override
+  Token ==>  sheep.t
+  import.name
+      @Ovveride
+   ProgrammingLanguage.t
+      TOKEN ==> SCRIPT
+   INPUT ==> 1000
+   PROGRAMMINGLANGAUGE ==> ASSEMBLY
+      @Ovveride 
+   Token.1000(programminglanguageManifest.xml)
+    Token.name(ovveride.token( ASSEMBLY.p)
+        server(data.gb)
+       Ram ==> programmingLanguage.p
+       Ram ==> data.p
+          Import.name
+      Input.token
+           Ram.language
+        import.os
+             Token.func(have.
