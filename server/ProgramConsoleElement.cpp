56ba437e4fa1c04f63cd7dda8830c1916b1205ba
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-12 15:28:41 +0300

    File

---
+++ server/ProgramConsoleElement.cpp
@@ -0,0 +1,116 @@
+#include <name>
+#include <key>
+#include <programming>
+    key.namespace(id)
+      func.tion(id)
+       programmingId.typename
+     namespace.id
+        ram.inline
+             programmingLanguageManifest
+      auto.id
+           space.id
+        typename(extern.
+         if(id.programmingLangauge.xml
+         id.mesh
+             func.tion
+          auto.keyword(setKey)
+         if(keySpinnet)
+             filetype=''d''
+      dll/system/spinnet/manifest
+          local.id/manifest
+      #; data.func(/spinnet/system/local/storage/Manifest.py)
+      #; dll.func.(spinnetId)
+            autoID.key
+  print(; keySpinnet
+     print(; a
+           key.namespace  }
+        clickButton.key
+          #;dll.id
+       namespace.keySpinnet
+          en.tion
+  {           print(; local
+  filenameSpace.(;) if.t_local
+         namespace.id
+           bit.name(console.id)
+       namespace.id
+          console.key(/system/data/namespace.cpp)
+        bit.data(console)
+            1; lo
+      928; spinnet.ml
+            namespace.enter }
+          spam.namespace
+             key.programmingLangauge
+ idkey.keyword 
+        9389;91 key
+   { 
+     idle.key
+       console.typename
+          /system/manifestlocal.cpp
+        key.id(robot.i)
+            idle.key
+          name.console(py)
+             console.id(/dl)
+           server.key(func)
+        filetype.
+             system ==> server
+               id ==> ram
+        ramname==func.id
+   }         {
+          robot.manifestlocal
+              manifest.cpp
+           system ==> request
+                idle.key ==> func.type
+              robot.manifestlocal
+          ram.pdb ==> idle.key(from/server)
+   }
+         ram.pdb ==> func.id(console.py)
+             idle ==> SYSTEM.ID
+       idle. ==> key
+           func.idle
+               hector.idle
+           system (id).name
+          namespace.id
+              keyword.system
+                id.idle
+             spinnet.(id).move
+         system.idle
+                    robot.manifestlocal
+                local.robot
+            console.func.(idle/server)
+                func.tion
+              filenameSpace.==> server
+       root.name(lc/system)
+             robto. ==> id
+    [[[[[id:template/system]]]
+    [[[[[id:template/system]]] ==> id local
+    [[[[[id:template/system]]]
+              [[[[[id:template/system]]]
+                [[[[[id:template/system]]]
+                robot.console
+            root.bot(console.py)
+         key.set(name.plex)
+                    [[[[[id:template/system]]]
+                      [[[[[id:template/system]]]
+             [[[[[id:template/system]]]
+             [[[[[id:template/system]]]
+                 [[[[[id:template/system]]]
+               [[[[[id:template/system]]]
+           template.system[[[[[id:key/system]]]]]
+    bot.root.system(/key/system)
+            [[[[[id:template/system]]]
+                [[[[[id:template/system]]]
+         idle.key(id)name
+          system.idle(000;local)
+       namespace.idle
+               name.download(key)
+               idle.(000; key)
+           template;system.idle
+       key.system.(; template)
+            bot.root(;000)
+         namespace;id(;000
+                download.template(;/system)
+              programmingLangaugeManifest.key
+   auto.key(namespace.key)
+         idle.(mesage.id)
+             template.console
+             
