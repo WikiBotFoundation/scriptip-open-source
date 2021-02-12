73a6f2f2a310435ae433e32d25cdbc0a43e4147d
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-12 14:53:11 +0300

    Create docs.java

---
+++ server/docs.java
@@ -0,0 +1,58 @@
+import.os
+import.data
+import.java
+import.name
+import.server
+import.doc
+     @Ovveride
+      class.name ==> private
+     ProgrammingLanguage ==> class
+           init(cal ==> doc
+       @Ovveride 
+           Open
+             init ==> doc.server
+         open.init
+             @Ovveride
+         console.doc( ==> server
+        init.call( ==> console
+             @Ovveride
+         doc.type( ==> name
+         filetype( ==> exe
+         filename( ==> ScriptIPlanucher
+             @Ovveride
+             doc.s
+                Export.File( ==> Thisprogramdisk
+           open.doc
+                file.lanucher( ==> exe
+           doc.file( ==> launch
+              ExportServer ==> studio
+          Filetype( ==> console
+                  init.form( ==> java
+       doc.type( ==> launch.server
+              @Ovveride
+         export.name ==> server.ProgrammingLangauge
+       p input == lexer.token
+             print(token ==> ja
+     input.token( ==> print.ja
+               jah.(over.ja
+            doc ==> type.ja(programmingLangauge
+        input (*6*8*6) ==> doc
+         Main.class. ==> call
+             doc.type( ==> data
+          82899 ==> Id.type
+                      export.serverFunc
+            Is.(if(Id.type
+               console.export(funF
+                 console.k
+         doc ==> type.k
+               rdfml.export(func 
+           func.doc(k)
+   run.data(console ==> export.i
+           Main.class(token ==> typeFile
+       
+
+
+
+
+
+     
