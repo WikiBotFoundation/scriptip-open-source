e282e264bcbcf8c5bac7d94a83f538b194010272
WikiBotFoundation <70801867+WikiBotFoundation@users.noreply.github.com>
2021-02-19 12:26:29 +0300

    Add files via upload

---
+++ server/MyLanguage.cpp
@@ -0,0 +1,25 @@
+#include <lang>
+#include <system>
+#include <java>
+  system.default( == key.lang)
+     java.register( == lang(;key)
+  system.files( == default.register )
+      
+  register.lang( ==> typename.keyword)
+        files.default( ; manifest.lang)
+    system.double(; files.lang)
+          language.default
+       print(keyword.system( ==> this.keyword)
+      this.keyword( ==> files(=={java})
+           double._Imaginary(; mySystem)
+       code.(; type::: files.system)
+           isLang.delete(; commands)
+     create(; command.file)
+       print(; keyword.manifest
+   system(::: java
+          default.typeid ::: manifest.system
+       double.command(; function : )
+             if(TokenCreated.:function)
+          token.file(; MySystem.manifest)
+      !(a-19), system.files !a(,i, 5,)
+           
